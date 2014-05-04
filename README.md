492ed4ce816f207a9ef767db966efbcb8b2d9b85
gaetan-lavenu <glavenu@MacGaetan.local>
2014-05-04 21:31:02 +0200

    Chargement initial

---
+++ README.md
@@ -0,0 +1,66 @@
+# Livre Cartographique
+
+Briefing Book is a configuration of ArcGIS and a JavaScript application that allows users to view and create map-based briefings and reports with interactive content.
+
+This application can be deployed by local, state, and national governments; utilities, NGOs, as well as private industries; to create and deliver dynamic and interactive briefings and reports for executives, decision-makers, knowledge-workers, and constituents.
+
+The Briefing Book can be used to create new briefings and reports that include ArcGIS maps, videos, photos, and written text. The briefings and reports are stored in your ArcGIS Online Organization or Portal for ArcGIS site and can be shared with members of your organization or the general public. The Briefing Book presents briefings and reports you author on a single bookshelf optimized for tablet devices.
+
+[Try the Briefing Book application](http://50.112.45.253/briefingbook/default.htm)
+
+[![Image of Briefing Book application](briefing-book.png "Briefing Book application")](http://50.112.45.253/briefingbook/default.htm)
+
+## Features
+
+* Create briefings and reports including ArcGIS maps, videos, photos, and written text
+* Share books with the public or individuals 
+
+## Instructions
+
+### General Help
+[New to Github? Get started here.](http://htmlpreview.github.com/?https://github.com/Esri/esri.github.com/blob/master/help/esri-getting-to-know-github.html)
+
+## Getting Started
+
+You'll need to configure and publish that application to get started:
+
+[Configure the application](http://solutions.arcgis.com/local-government/help/briefing-book/get-started/configure-application/)
+
+[Publish the application](http://solutions.arcgis.com/local-government/help/briefing-book/get-started/publish-application/)
+
+## Resources
+
+Learn more about Esri's [ArcGIS Solutions](http://solutions.arcgis.com/).
+
+Show me a list of other [Esri GitHub repositories](http://esri.github.io/).
+
+## Issues
+
+Find a bug or want to request a new feature?  Please let us know by submitting an issue.
+
+## Contributing
+
+Esri welcomes contributions from anyone and everyone.
+Please see our [guidelines for contributing](https://github.com/esri/contributing).
+
+## Licensing
+
+Copyright 2013 Esri
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+A copy of the license is available in the repository's
+[LICENSE.txt](LICENSE.txt) file.
+
+[](Esri Tags: Local-Government Local Government Briefing Book State-Government State Government ArcGIS Defense and Intelligence Military)
+[](Esri Language: JavaScript)
---
+++ nls/fr/localizedStrings.js
@@ -0,0 +1,110 @@
+/*global define */
+/*jslint browser:true,sloppy:true,nomen:true,unparam:true,plusplus:true */
+/*
+ | Copyright 2013 Esri
+ |
+ | Licensed under the Apache License, Version 2.0 (the "License");
+ | you may not use this file except in compliance with the License.
+ | You may obtain a copy of the License at
+ |
+ |    http://www.apache.org/licenses/LICENSE-2.0
+ |
+ | Unless required by applicable law or agreed to in writing, software
+ | distributed under the License is distributed on an "AS IS" BASIS,
+ | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ | See the License for the specific language governing permissions and
+ | limitations under the License.
+ */
+
+define({
+    noPages: "Aucune page trouvée",
+    tocContentsCaption: "Contenus",
+    homeTitle: "Accueil",
+    addPageTitle: "Ajouter une page",
+    alertDialogTitle: "Alerte",
+    confirmDialogTitle: "Confirmation",
+    downloadBookTitle: "Télécharge le livre",
+    refreshBookTitle: "Rafraîchir",
+    editTitle: "Modifier le livre",
+    deleteTitle: "Supprimer la Page",
+    descriptionNotAvailable: "Description non disponible",
+    removeBookTitle: "Supprimer le livre",
+    tocTitle: "Contenus",
+    addPageText: "Ajouter",
+    addBookTitle: "Nouveau livre",
+    saveBookShelf: "Enregistrer le livre",
+    copyBookShelf: "Copier le livre",
+    defaultItemId: "nouvelElement",
+    contentsPageTitle: "Contenus",
+    pageRequireMessage: "Un titre est nécessaire pour la page",
+    page: "Page",
+    of: "sur",
+    okButtonText: "OK",
+    saveButtonText: "Enregistrer",
+    cancelButtonText: "Annuler",
+    closeButtonTitle: "Fermer",
+    untitled: "Sans titre",
+    legendTitle: "Légende",
+    fullScreen: "Basculer en plein écran",
+    dndModuleText: "Glisser-Déposer des Modules ",
+    selectAnyLayout: "Sélectionner une mise en page ",
+    fieldIsEmpty: "Le champ ne peut pas être vide ",
+    webMapIconTitle: "Carte web",
+    freeFormIconTitle: "HTML",
+    textAreaIconTitle: "Zone de texte",
+    imageIconTitle: "Image",
+    videoIconTitle: "Video",
+    flickrIconTitle: "Flickr ",
+    editMentEditTitle: "Modifier",
+    editMentDeleteTitle: "Supprimer ",
+    confirmModuleDeleting: "Voulez-vous supprimer ce module ? ",
+    authorName: "Nom de l\'auteur",
+    settingDialogTitle: "Modifier ${modType}",
+    mapbookDefaultTitle: "Le titre du livre",
+    confirmPageDeleting: "Cette page va être supprimée. Cliquer sur \'OK\' pour confirmer.",
+    confirmCopyOfSelectedBook: "Souhaitez-vous copier ce livre ?",
+    confirmDeletingOfSelectedBook: "${bookName} va être supprimé. Cliquer sur \'OK\' pour confirmer la suppression.",
+    confirmAppReloading: "La bibliothèque de livres va être rechargée et vous perdrez vos modifications.",
+    validateBookOwner: "Vous n\'avez pas la permission de modifier ce livre.",
+    validateOrganizationUser: "Vous n\'êtes pas membre de cette organisation.",
+    copyKeyword: 'Copie de ',
+    signInText: "Se connecter",
+    signOutText: "Se déconnecter",
+    unitInPixel: " (px)",
+    unLockBookTitle: "Verrouiller le livre",
+    lockBookTitle: "Déverrouiller le livre",
+    selectedWebmapText: "Affichage actuel:  <b>${webmapName}</b>. Cliquer sur \'Selectionner une carte\' pour choisir une carte web",
+    selectWebmapBtnText: "Selectionner une carte",
+    selectWebmapDialogTitle: "Selectionner une carte web",
+    searchWebmapPlaceHolder: "Saisir les termes de la recherche",
+    validateUnSavedEdits: "Ce livre contient des modifications non enregistrées. Cliquer sur \'Annuler\' pour rester sur cette page. Cliquer sur \'OK\' pour continuer sans enregistrer.\nAttention : Si vous n\'enregistrez pas votre livre, toutes les modifications réalisées seront perdues.",
+    bookNotSaved: "Ce livre n\'est pas encore enregistré. Cliquer sur l\'icone \'Enregistrer\' pour sauvegarder ce livre.",
+    webmapCountStatus: "${start} - ${end} sur ${total}",
+    noWebmapFound: "Aucune carte trouvée",
+    loadingWebmap: "Chargement...",
+    shareBookTitle: "Partager ce livre",
+    shareBookDialogTitle: "Options de partage",
+    shareToOrgText: "Organisation",
+    shareToEveryoneText: "Tout le monde (public)",
+    protectCopyBookText: "Protéger ce livre contre la copie",
+    copyRestricted: "Impossible de copier, le propriétaire de ce livre l\'a protégé contre la copie.",
+    orientationNotSupported: "L\'application ne supporte pas cette orientation",
+    errorMessages: {
+        webmapSearchFailed: "Une erreur s\'est produite lors de la recherche de la carte",
+        widgetNotLoaded: "Widget non chargé ",
+        updatingItemError: "La mise à jour de l\'élément sélectionné a échouée",
+        contentPageRequired: "Page de contenu est requise ",
+        moduleFieldsEmpty: "Certains champs du module ne sont pas complétés",
+        invalidMapUrl: "URL de la carte web incorrecte ",
+        downloadZipFileFailed: "Le télé chargement a échoué ",
+        fieldInputIsNotValid: "Saisie invalide ",
+        addingItemError: "L\'ajout du  nouvel élément a échoué",
+        deletingItemError: "La suppression de l\'élément a échoué ",
+        updaingItemError: "La mise à jour de l\'élément a échoué",
+        copyItemError: "La copie de l\'élément sélectionné a échoué ",
+        contentQueryError: "Le chargement des contenus de l\'utilisateur a échoué",
+        webmapError: "Une erreur s\'est produite lors du chargement de la carte web ",
+        permissionDenied: "Vous n\avez pas la permission d\accéder à cette ressource.",
+        shareItemError: "Le partage de l\élément sélectionné a échoué"
+    }
+});
